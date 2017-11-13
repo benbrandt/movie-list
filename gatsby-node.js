f85235873a9a445ed07424643ebe8a8b8b7de9fc
Ben Brandt <benjamin.j.brandt@gmail.com>
2017-11-12 22:42:40 -0800

    Basic data structure

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+
+  return new Promise((resolve, reject) => {
+    const movieTemplate = path.resolve(`src/templates/movie.js`);
+    // Query for markdown nodes to use in creating pages.
+    resolve(
+      graphql(
+        `
+          {
+            allMoviesJson {
+              edges {
+                node
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) reject(result.errors);
+
+        // Create pages for each markdown file.
+        result.data.allMoviesJson.edges.forEach(({ node }) => {
+          const path = node.id;
+          createPage({
+            path,
+            component: movieTemplate,
+            // If you have a layout component at src/layouts/blog-layout.js
+            // layout: "movie",
+            // In your blog post template's graphql query, you can use path
+            // as a GraphQL variable to query for data from the markdown file.
+            context: {
+              id: node.id
+            }
+          });
+        });
+      })
+    );
+  });
+};
---
+++ src/templates/movie.js
@@ -0,0 +1,94 @@
+// @flow
+import React from "react";
+import Helmet from "react-helmet";
+import type { MovieT } from "../types";
+
+function runtime(mins: number) {
+  const hours = Math.floor(mins / 60);
+  const newMins = mins % (hours * 60);
+  return `${hours}hr${hours > 1 ? "s" : ""} ${newMins}min${
+    newMins > 1 ? "s" : ""
+  }`;
+}
+
+type PropsT = { data: { movie: MovieT } };
+export default class BlogPostTemplate extends React.Component<PropsT> {
+  render() {
+    const { movie, site } = this.props.data;
+
+    return (
+      <section
+        style={
+          movie.backdrop != null
+            ? {
+                backgroundImage: `url('https://image.tmdb.org/t/p/w1280${
+                  movie.backdrop
+                }')`
+              }
+            : { backgroundColor: "#333" }
+        }
+      >
+        <Helmet title={`${movie.title} | ${site.siteMetadata.title}`} />
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster || ""}`}
+            alt={movie.title}
+            style={{ width: "calc(100% / 3)" }}
+          />
+          <div>
+            <div className="description">
+              <h1>
+                {movie.title}
+                {movie.title !== movie.originalTitle && (
+                  <small>({movie.originalTitle})</small>
+                )}
+              </h1>
+              <h3>
+                {movie.releaseDate.substr(0, 4)}
+                {` / `}
+                {runtime(movie.runtime)}
+              </h3>
+              {/* <Rankings rankings={movie.ranking} /> */}
+              {movie.tagline != null && <h5>{movie.tagline}</h5>}
+              <p>{movie.overview}</p>
+              <span className="tmdb">
+                Film data from <a href="https://www.themoviedb.org/">TMDb</a>.
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query MovieById($id: ID!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    movie(fields: { id: { eq: $id } }) {
+      id
+      backdrop
+      originalTitle
+      overview
+      poster
+      releaseDate
+      runtime
+      tagline
+      title
+      ranking {
+        bfi
+        imdb
+        letterboxd
+        metacritic
+        mubi
+        rottenTomatoes
+        tmdb
+      }
+    }
+  }
+`;
