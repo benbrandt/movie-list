af6fb67ce4b7753194a972537e46b5a2b2151f08
Ben Brandt <benjamin.j.brandt@gmail.com>
2017-11-13 22:14:53 -0800

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,98 @@
+// @flow
+/* eslint-disable no-undef */
+import GatsbyLink from "gatsby-link";
 import React from "react";
-import Link from "gatsby-link";
+import styled from "react-emotion";
+import slugify from "slugify";
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+type Props = {
+  data: {
+    allMoviesJson: {
+      edges: Array<{
+        node: {
+          id: string,
+          poster: ?string,
+          releaseDate: string,
+          title: string
+        }
+      }>
+    }
+  }
+};
+
+const Link = styled(GatsbyLink)`
+  overflow: hidden;
+  position: relative;
+  text-decoration: none;
+`;
+
+const Section = styled("section")`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Poster = styled("div")`
+  -moz-osx-font-smoothing: grayscale;
+  backface-visibility: hidden;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover !important;
+  padding-bottom: 150%;
+  transform: translateZ(0);
+  transition: transform 0.25s ease-out;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const Position = styled("span")`
+  background-color: rgba(255, 255, 255, 0.9);
+  color: #000;
+  height: 1rem;
+  left: 0;
+  position: absolute;
+  text-align: center;
+  top: 0;
+  width: 2rem;
+`;
+
+export default ({ data: { allMoviesJson: { edges } } }: Props) => (
+  <Section>
+    {edges.map(({ node: { id, title, poster, releaseDate } }, index) => (
+      <Link key={id} to={`/${slugify(`${title} ${releaseDate.substr(0, 4)}`)}`}>
+        <Poster
+          title={title}
+          style={{
+            background:
+              poster != null
+                ? `url('https://image.tmdb.org/t/p/w185${poster}')`
+                : "black"
+          }}
+        />
+        <Position>{index + 1}</Position>
+      </Link>
+    ))}
+  </Section>
 );
 
-export default IndexPage;
+// $FlowFixMe
+export const query = graphql`
+  query AllMovies {
+    allMoviesJson(sort: { fields: position, order: DESC }) {
+      edges {
+        node {
+          id
+          poster
+          releaseDate
+          title
+        }
+      }
+    }
+  }
+`;
